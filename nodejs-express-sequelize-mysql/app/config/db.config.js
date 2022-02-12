96b98035848f17e304e0fd7652e96eac81b6ff26
sorawich-s63 <s6301012630176@email.kmutnb.ac.th>
2022-02-12 15:48:25 +0700

    run build

--- nodejs-express-sequelize-mysql/app/config/db.config.js
+++ nodejs-express-sequelize-mysql/app/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: "us-cdbr-east-05.cleardb.net",
     USER: "b4e86179a0a68e",
     PASSWORD: "b2a3db63",
-    DB: "heroku_1bc3ef3ee27a279?",
+    DB: "heroku_1bc3ef3ee27a279",
     dialect: "mysql",
     pool: {
         max: 5,
